c2dd45b9dc357a3ac21f6229c821ae55f7cb8221
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-10-25 01:00:42 -0700

    Update bc_transit_victoria_tracker.py

--- bc_transit_victoria_tracker.py
+++ bc_transit_victoria_tracker.py
@@ -184,7 +184,7 @@ def generate_map(buses, bus_number, current_trips, trips_df, stops_df):
             stop_text = f"First Stop: {stop}"
     else:
         trip_headsign = trip_headsign.iloc[0]
-        desc_text = f"{bus_id} is running the {route_number} {trip_headsign}"
+        desc_text = f"{bus_id} is currently {delay} late running the {route_number} {trip_headsign}"
         if speed > 0:
             stop_text = f"Next Stop: {stop}"
         else:
