bdd91c955c3fcdc5f958f364b5169d434a0358a6
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-10-25 21:32:43 -0700

    Update bc_transit_victoria_tracker.py

--- bc_transit_victoria_tracker.py
+++ bc_transit_victoria_tracker.py
@@ -107,7 +107,7 @@ def generate_map(buses, bus_number, current_trips, trips_df, stops_df):
     lat, lon, speed, route, bus_id, capacity, trip_id, stop_id, bearing, timestamp = (
         bus["lat"], bus["lon"], bus["speed"], bus["route"], bus["id"][6:], bus["capacity"], bus["trip_id"], bus["stop_id"], bus["bearing"], bus["timestamp"]
     )
-    current_trip = next((trip for trip in current_trips if trip["trip_id"] == trip_id), None)
+    current_trip = [trip for trip in current_trips if trip["trip_id"] == trip_id]
     current_stop = next((stop for stop in current_trip if stop["stop_id"] == stop_id), None)
 
     deadheading = False
