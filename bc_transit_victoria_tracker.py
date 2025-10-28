c1ee3742f58c38d9d5b0d2cb868c4ff97bb5e01e
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-10-27 21:35:45 -0700

    Update bc_transit_victoria_tracker.py

--- bc_transit_victoria_tracker.py
+++ bc_transit_victoria_tracker.py
@@ -140,7 +140,7 @@ def generate_map(buses, bus_number, current_trips, trips_df, stops_df, toggle_fu
     else:
         stop_times_df = load_stop_times(trip_id)
         current_trip_stop_ids = stop_times_df["stop_id"].astype(float).tolist()
-        current_trip_stops_df = stops_df[stops_df["stop_id"].isin(trip_stop_ids)]
+        current_trip_stops_df = stops_df[stops_df["stop_id"].isin(current_trip_stop_ids)]
         fig.add_trace(go.Scattermapbox(
             lat=trip_stops_df["stop_lat"],
             lon=trip_stops_df["stop_lon"],
