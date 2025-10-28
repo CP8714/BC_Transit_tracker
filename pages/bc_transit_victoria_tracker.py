4baf2d53e6319bb1784383fa4555f2b58d526cad
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-10-28 16:47:26 -0700

    Update bc_transit_victoria_tracker.py

--- pages/bc_transit_victoria_tracker.py
+++ pages/bc_transit_victoria_tracker.py
@@ -3,7 +3,7 @@ import geopandas as gpd
 import json
 import dash
 from dash import html, dcc, register_page, callback
-from dash.dependencies import Output, Input
+from dash.dependencies import Output, Input, State
 # import plotly.express as px
 import plotly.graph_objects as go
 import math
