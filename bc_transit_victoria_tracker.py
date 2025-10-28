8de7aa6b43c10d6151fb55c8f3355a2bcf2f2fa6
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-10-28 09:48:37 -0700

    Update bc_transit_victoria_tracker.py

--- bc_transit_victoria_tracker.py
+++ bc_transit_victoria_tracker.py
@@ -327,6 +327,7 @@ from dash import callback_context
      prevent_initial_call=True
 )
 def show_loading_text(manual_update, search_for_bus, bus_number):
+    triggered_id = callback_context.triggered_id
     if triggered_id == "manual-update":
         return f"Getting latest data on {bus_number}"
     else:
