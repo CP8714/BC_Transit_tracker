eabe324f60be484c9c392f87cebfe44469354fa3
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-10-28 15:52:44 -0700

    Update next_buses.py

--- pages/next_buses.py
+++ pages/next_buses.py
@@ -4,6 +4,6 @@ from dash import html, dcc, register_page, callback
 dash.register_page(__name__, path="/next_buses")
 
 layout = html.Div([
-    html.H1("Next Busses Page"),
+    html.H1("Next Buses Page"),
     dcc.Link("← Back to Bus Tracker", href="/"),
 ])
