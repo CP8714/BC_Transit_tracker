e5a96cc96a2ae8ab51f93e49d32702c193bf1729
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-10-28 18:17:23 -0700

    Update next_buses.py

--- pages/next_buses.py
+++ pages/next_buses.py
@@ -1,6 +1,8 @@
 import dash
 from dash import html, dcc, register_page, callback
 from dash.dependencies import Output, Input, State
+import fetch_fleet_data
+import fetch_trip_data
 
 dash.register_page(__name__, path="/next_buses")
 
