d5ebdf4396e03acf0d5b627f304758415cbaa013
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-10-28 20:31:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from dash import Dash, dcc, html
 import dash
 
-app = Dash(__name__, use_pages=True)
+app = dash.Dash(__name__, use_pages=True)
 
 app.layout = dash.page_container
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run_server(debug=True)
