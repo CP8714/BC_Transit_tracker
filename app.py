0345e7a1edec81c635bc96f8f3e82837c9bad3ec
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-10-28 20:25:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ import dash
 
 app = Dash(__name__, use_pages=True)
 
-app.layout = html.Div([
-    dash.page_container
-])
+app.layout = dash.page_container
 
 if __name__ == "__main__":
     app.run(debug=True)
