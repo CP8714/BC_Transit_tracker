8d976de4e9a1a5708505d52a3a2281bf99768c57
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-10-28 22:13:59 -0700

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from dash import Dash, dcc, html
-import dash
-
-app = dash.Dash(__name__, use_pages=True)
-
-app.layout = dash.page_container
-
-if __name__ == "__main__":
-    app.run_server(debug=True)
