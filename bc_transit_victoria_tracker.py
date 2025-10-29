ab5a86579d07321a533d8e297b143e89e1728d39
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-10-28 23:06:30 -0700

    Update bc_transit_victoria_tracker.py

--- bc_transit_victoria_tracker.py
+++ bc_transit_victoria_tracker.py
@@ -171,10 +171,11 @@ def get_capacity(capacity):
 def get_next_buses(stop_number, stops_df):
     if not stop_number:
         return "No Stop Number Entered", ""
-    stop_number = int(stop_number)
+    stop_number = float(stop_number)
     stop = stops_df.loc[stops_df["stop_id"] == stop_number, "stop_name"]
     if stop.empty:
-        return "Entered Stop Number is not valid", ""
+        return f"{stop_number} is not a valid Stop Number", ""
+    stop_number = float(stop_number)
     stop_name = stop.iloc[0]
     stop_name_text = f"Next Buses For Stop {stop_number:d} ({stop_name})"
     return stop_name_text, "Hello"
