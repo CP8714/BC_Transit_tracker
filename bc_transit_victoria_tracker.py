863fcd93f72a122eae6ca922f47e3fd62fbd7221
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-11-01 10:38:52 -0700

    Update bc_transit_victoria_tracker.py

--- bc_transit_victoria_tracker.py
+++ bc_transit_victoria_tracker.py
@@ -698,7 +698,7 @@ def update_stop_callback(n_intervals, manual_update, look_up_next_buses, look_up
     reset_url = no_update
 
     # Check if there is a stop number in the url and use it if so
-    if href:
+    if href and "/next_buses" in href:
         parsed_url = urlparse(href)
         query_params = parse_qs(parsed_url.query)
         if "stop_id" in query_params:
