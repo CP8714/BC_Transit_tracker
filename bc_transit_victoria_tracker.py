38b5664f91ab6a569d0fc70d401d08dfcf011f88
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-11-05 05:11:35 -0800

    Update bc_transit_victoria_tracker.py

--- bc_transit_victoria_tracker.py
+++ bc_transit_victoria_tracker.py
@@ -638,7 +638,9 @@ def display_page(pathname):
      Input("search-for-bus", "n_clicks"),
      Input("toggle-future-stops", "n_clicks"),
      Input("url", "href"),
-     Input("clear-bus-input", "n_clicks")],
+     Input("clear-bus-input", "n_clicks"),
+     Input("stop-interval-component", "n_intervals"),
+     Input("stop-search", "n_clicks")],
     [State("bus-search-user-input", "value")]
 )
 def update_bus_callback(bus_n_intervals, manual_update, search_for_bus, toggle_future_stops_clicks, href, clear_bus_input, stop_n_intervals, stop_search, toggle_future_buses_clicks, bus_number, stop_number_input, route_number_input):
