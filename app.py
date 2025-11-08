40e6aaab8fba15b67aed0e908756c77f29dd0db0
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-11-08 04:54:55 -0800

    Update app.py

--- app.py
+++ app.py
@@ -85,8 +85,13 @@ bus_tracker_layout = html.Div([
             id="loading-component-map",
             type="circle",
             children=[
-                # Map which shows the bus' currently location as well as the route it is currently running and which stops it is serving
-                dcc.Graph(id="live-map"),
+                # Map which shows the bus' current location as well as the route it is currently running and which stops it is serving
+                html.Div(
+                    dcc.Graph(
+                        id="live-map"
+                    ),
+                    className="map-container"
+                )
                 dcc.Interval(id="resize-interval", interval=1000, n_intervals=0, max_intervals=1),
                 html.Div([
                     html.Label("Enter Bus Number:"),
