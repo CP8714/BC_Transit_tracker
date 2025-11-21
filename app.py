da2ff98d3a3cfe7844fb879bdff548eb1425f217
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-11-20 17:22:20 -0800

    Update app.py

--- app.py
+++ app.py
@@ -365,7 +365,12 @@ def load_block_departure_times(trip_ids):
     stop_times_file = os.path.join("data", "stop_times.csv")
     departure_times_list = []
     if os.path.exists(stop_times_file):
-        departure_times_chunks = pd.read_csv(stop_times_file, chunksize=10000, usecols=["trip_id", "stop_sequence", "departure_time"])
+        departure_times_chunks = pd.read_csv(
+            stop_times_file,
+            chunksize=10000,
+            usecols=["trip_id", "stop_sequence", "departure_time"],
+            encoding="utf-8-sig"
+        )
         for departure_times_chunk in departure_times_chunks:
             departure_times = departure_times_chunk[departure_times_chunk["trip_id"].isin(trip_ids) & (departure_times_chunk["stop_sequence"] == 1)]
             if not departure_times.empty:
