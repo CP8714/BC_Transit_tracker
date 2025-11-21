57c680bef16c6ceae80580296b6fe5c0a0a97c73
CP8714 <76891553+CP8714@users.noreply.github.com>
2025-11-20 18:09:53 -0800

    Update app.py

--- app.py
+++ app.py
@@ -366,7 +366,7 @@ def load_block_departure_times(trip_ids):
     departure_times_list = []
     if os.path.exists(stop_times_file):
         test = pd.read_csv(stop_times_file, nrows=0, encoding="utf-8-sig")
-        print(test.columns.tolist())
+        print("COLUMNS LOADED:", test.columns.tolist())
         departure_times_chunks = pd.read_csv(
             stop_times_file,
             chunksize=10000,
